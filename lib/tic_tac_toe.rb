7f1b16eeef6dda143e51666a9bb1746c90f75e51
dmadust@gmail.com <dmadust@gmail.com>
2018-05-11 04:38:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,5 +73,16 @@ class TicTacToe
     end
   end
   
+  def turn
+    puts "Specify the space you'd like to play (1-9)"
+    input = gets.strip
+    index = input_to_index(index)
+    until valid_index?(index)
+      input = gets.strip
+      index = input_to_index(index)
+    end
+    move(index, current_player)
+  end
+  
   
 end
